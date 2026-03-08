fe95b70c6f05e4d11c516e058dc95b7798a89792
Sneha B Poojary <snehapoojary789@gmail.com>
2026-03-08 21:16:21 +0530

    Add files via upload

---
+++ Array/reverse.py
@@ -0,0 +1,16 @@
+#Reverse an array arr[]. Reversing an array means rearranging the elements such that the first element becomes the last,
+#the second element becomes second last and so on
+
+# def reverse_array(arr):
+#     start = 0
+#     end = len(arr)-1
+#     while start <end:
+#         arr[start], arr[end] = arr[end], arr[start]
+#         start += 1
+#         end -= 1
+#     return arr
+# arr = [1, 2, 3, 4, 5]
+# print(reverse_array(arr))
+
+# arr = [1, 2, 3, 4, 5,6]
+# print(arr[::-1])
